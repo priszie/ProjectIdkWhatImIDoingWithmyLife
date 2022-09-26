e92b0d7980752e1e5c5feffd841960f3c1054a79
Priszie <pitchayamaha@gmail.com>
2022-09-26 09:34:31 +0700

    priszie

--- lab6_game1/Game1.cs
+++ lab6_game1/Game1.cs
@@ -15,25 +15,20 @@ namespace lab6_game1
         Vector2 ballPosition = new Vector2(250, 250);
         Random r = new Random();
         private AnimatedTexture SpriteTexture;
-
         // Ball Array
         Vector2[] BallPosition = new Vector2[4];
         int[] BallColor = new int[4];
-
         private const float Rotation = 0;
         private const float Scale = 1.0f;
         private const float Depth = 0.5f;
-
         private const int Frames = 4;
         private const int FramesPerSec = 15;
         private const int FramesRow = 4;
         int FrameToRun = 1;
-
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
             SpriteTexture = new AnimatedTexture(Vector2.Zero, Rotation, Scale, Depth);
-
             Content.RootDirectory = "Content";
         }
         protected override void Initialize()
@@ -45,14 +40,14 @@ namespace lab6_game1
             spriteBatch = new SpriteBatch(GraphicsDevice);
             SpriteTexture.Load(Content, "Char01", Frames, FramesRow, FramesPerSec);
             ballTexture = Content.Load<Texture2D>("ball");
-
             for (int i = 0; i < BallPosition.Length; i++)
             {
                 BallPosition[i] = new Vector2(r.Next(0, 500), r.Next(100, 350));
-                BallColor[i] = r.Next(0,3);
+                BallColor[i] = r.Next(0, 3);
             }
-
         }
+
+
         protected override void UnloadContent()
         {
             // TODO: Unload any non ContentManager content here
